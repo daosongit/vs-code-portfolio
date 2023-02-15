4ac3a078d967300b7fcf8e5623a749f341be337a
Ivan Kulai <daosonmail@protonmail.com>
2023-02-15 16:20:44 +0200

    Added Explorer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useMemo } from 'react';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme/theme';
+import Home from './pages/Home/Home';
 
 export default function App() {
   const th = useMemo(() => theme('light'), []);
 
-  return <ThemeProvider theme={th}>color</ThemeProvider>;
+  return (
+    <ThemeProvider theme={th}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
+  );
 }
---
+++ src/pages/Home/components/Explorer.tsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Collapse,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { IoIosArrowForward as IcoArrowLeft, IoIosArrowDown as IcoArrowDown } from 'react-icons/io';
+import { RiReactjsFill as IcoReact } from 'react-icons/ri';
+
+export default function Explorer() {
+  const [open, setOpen] = useState(true);
+  const explorerList = [
+    { title: 'Portfolio', icon: <IcoReact />, link: '#' },
+    { title: 'Snake', icon: <IcoReact />, link: '#' },
+  ];
+  return (
+    <Box
+      component="aside"
+      color="text.secondary"
+      sx={{ width: { xs: '100%', xm: '200px' }, bgcolor: 'custom.explorer' }}>
+      <List
+        component="nav"
+        sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}
+        subheader={
+          <ListSubheader component="span" sx={{ bgcolor: 'transparent' }}>
+            Explorer
+          </ListSubheader>
+        }>
+        <ListItemButton
+          component="button"
+          disableRipple
+          sx={{ columnGap: '10px', width: '100%', py: '5px' }}
+          onClick={() => setOpen(!open)}>
+          <Box component="span" display="flex">
+            {open ? <IcoArrowDown /> : <IcoArrowLeft />}
+          </Box>
+          <Typography component="h6" variant="h6">
+            Home
+          </Typography>
+        </ListItemButton>
+        <Collapse in={open}>
+          <List disablePadding>
+            {explorerList.map((el) => (
+              <ListItemButton
+                component="a"
+                href={el.link}
+                disableRipple
+                sx={{ pl: '30px', width: '100%', columnGap: '5px', py: '5px' }}>
+                <Box component="span" display="flex" paddingLeft="10px" color="#4c96eb">
+                  {el.icon}
+                </Box>
+                <Typography component="span" variant="body1">
+                  {el.title}
+                </Typography>
+              </ListItemButton>
+            ))}
+          </List>
+        </Collapse>
+      </List>
+    </Box>
+  );
+}

a5069e4bda03c552f11a16a5ff1d9a469a5d45c5
Ivan Kulai <daosonmail@protonmail.com>
2023-02-15 21:41:53 +0200

    Use component customization instead of array.map for more flexibility

--- src/pages/Home/components/ActivityBar.tsx
+++ src/pages/Home/components/ActivityBar.tsx
@@ -3,16 +3,38 @@ import { ImFilesEmpty as IcoExplorer } from 'react-icons/im';
 import { IoIosGitBranch as IcoGit } from 'react-icons/io';
 import { CiLight as IcoLight, CiDark as IcoDark } from 'react-icons/ci';
 import { useAppDispatch, useAppSelector } from '../../../redux/store';
+import { FC, ReactElement } from 'react';
 import { changeTheme } from '../../../redux/themeSlice';
+
+interface MenuItemProps {
+  title: string;
+  onClick?: () => void;
+  children?: ReactElement;
+}
 
 export default function ActivityBar() {
   const theme = useAppSelector((state) => state.theme.state);
   const dispatch = useAppDispatch();
-  const menuList = [
-    { title: 'Explorer', icon: <IcoExplorer size={27} /> },
-    { title: 'GitHub', icon: <IcoGit size={30} /> },
-    { title: 'Theme', icon: theme === 'light' ? <IcoLight size={33} /> : <IcoDark size={33} /> },
-  ];
+  const MenuItem: FC<MenuItemProps> = ({ title, children, onClick = () => {} }) => {
+    return (
+      <Tooltip title={title} placement="right" describeChild arrow disableTouchListener>
+        <Button
+          onClick={onClick}
+          sx={{
+            p: 0,
+            minWidth: 0,
+            color: '#a0a0a0',
+            borderLeft: '1px solid transparent',
+            borderRadius: 0,
+            '&:hover': { color: { md: '#ffffff' }, bgcolor: 'transparent' },
+          }}
+          disableRipple>
+          {children}
+        </Button>
+      </Tooltip>
+    );
+  };
+
   return (
     <Box
       component="aside"
@@ -23,36 +45,19 @@ export default function ActivityBar() {
         top: 0,
         width: '50px',
         height: '100vh',
-        bgcolor: 'custom.activityBar',
+        bgcolor: '#333333',
         pt: '20px',
         rowGap: '15px',
       }}>
-      {menuList.map((el) => {
-        const onClick = el.title === 'Theme' ? () => dispatch(changeTheme()) : () => {};
-        return (
-          <Tooltip
-            key={el.title}
-            title={el.title}
-            placement="right"
-            describeChild
-            arrow
-            disableTouchListener>
-            <Button
-              onClick={onClick}
-              sx={{
-                p: 0,
-                minWidth: 0,
-                color: '#a0a0a0',
-                borderLeft: '1px solid transparent',
-                borderRadius: 0,
-                '&:hover': { color: { md: '#ffffff' }, bgcolor: 'transparent' },
-              }}
-              disableRipple>
-              {el.icon}
-            </Button>
-          </Tooltip>
-        );
-      })}
+      <MenuItem title="Explorer">
+        <IcoExplorer size={27} />
+      </MenuItem>
+      <MenuItem title="GitHub">
+        <IcoGit size={30} />
+      </MenuItem>
+      <MenuItem title="Theme" onClick={() => dispatch(changeTheme())}>
+        {theme === 'light' ? <IcoLight size={33} /> : <IcoDark size={33} />}
+      </MenuItem>
     </Box>
   );
 }

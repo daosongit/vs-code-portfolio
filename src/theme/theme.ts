4eda25570c3bd11de8c6ca9d662944d9bb1c569d
Ivan Kulai <daosonmail@protonmail.com>
2023-02-15 16:19:18 +0200

    Added breakpoint xm:400

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,12 +3,28 @@ import { createTheme } from '@mui/material/styles';
 const theme = (mode: string) => {
   const isLight = mode === 'light'
   const options = {
+    breakpoints:{
+      keys: ['xs', 'xm', 'sm', 'md', 'lg', 'xl'],
+      values: {
+        xs: 0,
+        xm: 400,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1536
+
+      }
+    },
     palette: {
+      custom:{
+        activityBar: '#333333',
+        explorer: isLight ? '#f3f3f3' : '#252527',
+      },
       primary: {
-        main: isLight ?  '#ffffff' : '#1e1e1e',
+        main: isLight ? '#1e1e1e' : '#ffffff',
       },
       secondary:{
-        main: isLight ? '#ffffff':'#1e1e1e' 
+        main: isLight ? '#1e1e1e' : '#ffffff',
       },
       background:{
         default: isLight ? '#ffffff' : '#1e1e1e' 

690568148c15a6c0406408f2e7771b1bf65a20bb
Ivan Kulai <daosonmail@protonmail.com>
2023-02-15 21:39:40 +0200

    Removed:  - keys  - activityBar color (it's never change)

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = (mode: string) => {
-  const isLight = mode === 'light'
+  const isLight = mode === 'light';
   const options = {
     breakpoints: {
       // keys: Array('xs', 'xm', 'sm', 'md', 'lg', 'xl'),
@@ -11,31 +11,32 @@ const theme = (mode: string) => {
         sm: 600,
         md: 900,
         lg: 1200,
-        xl: 1536
-
-      }
+        xl: 1536,
+      },
     },
     palette: {
-      custom:{
-        activityBar: '#333333',
+      custom: {
         explorer: isLight ? '#f3f3f3' : '#252527',
       },
       primary: {
         main: isLight ? '#1e1e1e' : '#ffffff',
       },
-      secondary:{
+      secondary: {
         main: isLight ? '#1e1e1e' : '#ffffff',
       },
-      background:{
-        default: isLight ? '#ffffff' : '#1e1e1e' 
+      background: {
+        default: isLight ? '#ffffff' : '#1e1e1e',
       },
-      text:{
-        secondary: isLight ? '#6a6760' : '#b8beca'
-      }
+      text: {
+        secondary: isLight ? '#6a6760' : '#b8beca',
+      },
+      action: {
+        hover: isLight ? 'rgba(0, 0, 0, 0.04)' : 'rgba(255, 255, 255, 0.04)',
+      },
     },
-  }
+  };
 
-  return  createTheme(options) 
-}
+  return createTheme(options);
+};
 
 export default theme;

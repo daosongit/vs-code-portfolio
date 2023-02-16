9e3cdb31c071ccc15126370b08844a2bef3c8629
Ivan Kulai <daosonmail@protonmail.com>
2023-02-16 18:48:33 +0200

    Removed Explorer from Root (now it's in the ActivityBar). Added empty Git page

---
+++ src/pages/Git/Git.tsx
@@ -0,0 +1,3 @@
+export default function Git() {
+  return <div>Git</div>;
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,8 @@
 import { Box, CssBaseline, ThemeProvider } from '@mui/material';
+import React from 'react';
 import { useMemo } from 'react';
 import { createHashRouter, useOutlet } from 'react-router-dom';
+import Git from '../pages/Git/Git';
 import ActivityBar from '../pages/Home/ActivityBar';
 import Explorer from '../pages/Home/Explorer';
 import Portfolio from '../pages/Portfolio/Portfolio';
@@ -9,15 +11,16 @@ import { useAppSelector } from '../redux/store';
 import theme from '../theme/theme';
 
 const Root = () => {
+  console.log('Root');
   const outlet = useOutlet();
   const themeMode = useAppSelector((state) => state.theme.state);
   const th = useMemo(() => theme(themeMode), [themeMode]);
+
   return (
     <ThemeProvider theme={th}>
       <Box display="flex">
         <CssBaseline />
         <ActivityBar />
-        <Explorer />
         {outlet}
       </Box>
     </ThemeProvider>
@@ -36,6 +39,10 @@ const router = createHashRouter([
       {
         path: '/snake',
         element: <Snake />,
+      },
+      {
+        path: '/git',
+        element: <Git />,
       },
     ],
   },
